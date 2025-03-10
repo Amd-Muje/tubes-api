b91a69567fb5a2a60177650bf65a54f24e4fa8f2
RasyaRahmatSyaban <rasyarahmatsyban10@gmail.com>
2025-03-10 20:38:41 +0000

    update users, donations, campaigns controller

--- app/controllers/donations_controller.ts
+++ app/controllers/donations_controller.ts
@@ -1,56 +1,147 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Donation from '#models/donation'
+import User from '#models/user'
+import Campaign from '#models/campaign'
+import Transaction from '#models/transaction'
+import db from '@adonisjs/lucid/services/db'
 
 export default class DonationsController {
     // Get All Donations
-    public async index({ response }: HttpContext) {
-        const donations = await Donation.all()
-        return response.json(donations)
+    public async index({ response, auth }: HttpContext) {
+        try {
+            const user = auth.user
+            const donations = await Donation.query().preload('user').preload('campaign')
+            // Jika user yang get all
+            if(user?.role !== 'admin') {
+                return response.json({ donations: donations.map(donation => ({ OwnerCampaign: donation.user.name, campaign: donation.campaign.title, amount: donation.amount, paymentStatus: donation.paymentStatus, paymentMethod: donation.paymentMethod })) })
+            }
+            // Admin get All
+            return response.json({donations: donations.map(donation => ({
+                id: donation.id,
+                OwnerCampaign: donation.user?.name,
+                userId: donation.userId,
+                campaign: donation.campaign.title,
+                amount: donation.amount,
+                paymentStatus: donation.paymentStatus,
+                paymentMethod: donation.paymentMethod,
+                createdAt: donation.createdAt,
+                updateAt : donation.updatedAt
+            }))})
+        } catch (error) {
+            return response.status(500).json({ message: 'Internal server error' })
+        }
     }
 
     // Get Donation By Id
-    public async show({ params, response }: HttpContext) {
-        const donation = await Donation.find(params.id)
-        if(!donation) {
-            return response.status(404).json({ message: 'Donation not found' })
+    public async show({ params, auth, response }: HttpContext) {
+        try {
+            const user = auth.user
+            const donation = await Donation.query().where('id', params.id).preload('user').preload('campaign').first()
+            if(!donation) {
+                return response.status(404).json({ message: 'Donation not found' })
+            }
+            if(user?.role !== 'admin') {
+                return response.json({ donation: { OwnerCampaign: donation.user.name, campaign: donation.campaign.title, amount: donation.amount, paymentStatus: donation.paymentStatus, paymentMethod: donation.paymentMethod } })
+            }
+            return response.json({donation: {
+                id: donation.id,
+                OwnerCampaign: donation.user?.name,
+                userId: donation.userId,
+                campaign: donation.campaign.title,
+                amount: donation.amount,
+                paymentStatus: donation.paymentStatus,
+                paymentMethod: donation.paymentMethod,
+                createdAt: donation.createdAt,
+                updateAt : donation.updatedAt
+            }})
+        } catch (error) {
+            return response.status(500).json({ message: 'Internal server error' })
         }
-        return response.json(donation)
     }
 
     // Create Donation
     public async store({ request, response }: HttpContext) {
-        const data = request.only(['userId', 'campaignId', 'amount', 'paymentMethod'])
-        const donation = await Donation.create({
-            ...data,
-            paymentStatus: 'pending',
-            transactionId: null,
-        })
+        const trx = await db.transaction()
+        try {
+            const data = request.only(['userId', 'campaignId', 'amount', 'paymentMethod'])
+            const user = await User.find(data.userId)
+            if(!user) {
+                await trx.rollback()
+                return response.status(404).json({ message: 'User not found' })
+            }
+            const campaign = await Campaign.find(data.campaignId)
+            if(!campaign) {
+                await trx.rollback()
+                return response.status(404).json({ message: 'Campaign not found' })
+            }
+            const donation = await Donation.create({
+                ...data,
+                paymentStatus: 'pending'
+            }, { client: trx })
 
-        return response.status(201).json({message : "Donation Created Succesfully", donation})
+            campaign.merge
+            ({
+                collectedAmount: campaign.collectedAmount + data.amount
+            })
+            await campaign.useTransaction(trx).save()
+
+            const transaction = await Transaction.create({
+                donationId: donation.id,
+                paymentMethod: data.paymentMethod,
+                status: 'pending'
+            }, { client: trx })
+
+            await trx.commit()
+
+            return response.status(201).json({message : "Donation Created Succesfully", donation, transaction})
+        } catch (error) {
+            await trx.rollback()
+            return response.status(500).json({ message: 'Internal server error' })
+        }
     }
 
     // Update Donation
-    public async update({ params, request, response }: HttpContext) {     
-        const donation = await Donation.find(params.id)
-        if(!donation) {
-            return response.status(404).json({ message: 'Donation not found' })
+    public async update({ params, auth, request, response }: HttpContext) {     
+        try {
+            const user = auth.user
+            const donation = await Donation.find(params.id)
+            if(!donation) {
+                return response.status(404).json({ message: 'Donation not found' })
+            }
+            if(user?.role !== 'admin'){
+                return response.status(400).json({message: "You not allowed to update donations"})
+            }
+    
+            const {paymentStatus} = request.only(['paymentStatus'])
+            if (!['pending', 'success', 'failed'].includes(paymentStatus)) {
+                return response.status(400).json({ message: 'Invalid payment status' })
+            }
+
+            donation.paymentStatus = paymentStatus;
+            await donation.save()
+    
+            return response.json({message : "Donation Updated",donation})
+        } catch (error) {
+            return response.status(500).json({ message: 'Internal server error' })
         }
-
-        const data = request.only(['amount', 'paymentStatus', 'paymentMethod', 'transactionId'])
-        donation.merge(data)
-        await donation.save()
-
-        return response.json({message : "Donation Updated",donation})
     }
 
     // Delete Donation
-    public async destroy({ params, response }: HttpContext) {
-        const donation = await Donation.find(params.id)
-        if(!donation) {
-            return response.status(404).json({ message: 'Donation not found' })
+    public async destroy({ params, auth, response }: HttpContext) {
+        try {
+            const user = auth.user
+            const donation = await Donation.find(params.id)
+            if(!donation) {
+                return response.status(404).json({ message: 'Donation not found' })
+            }
+            if(user?.role !== 'admin') {
+                return response.status(403).json({ message: 'You are not authorized to delete this donation' })
+            }
+            await donation.delete()
+    
+            return response.status(202).json({message : "Donation Deleted"})
+        } catch (error) {
+            return response.status(500).json({ message: 'Internal server error' })
         }
-        await donation.delete()
-
-        return response.status(204).json({message : "Donation Deleted"})
     }
 }
--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -5,25 +5,38 @@ import hash  from '@adonisjs/core/services/hash'
 export default class UsersController {
     // Get All Users
     public async index({ auth, response }: HttpContext) {
-        const user = await User.find(auth.user?.id)
-        if(user?.role !== 'admin') {
-            return response.status(403).json({ message: 'Only admin can access this resource' })
-        }    
-        const users = await User.all()
-        return response.status(201).json(users)
+        try {
+            const user = auth.user
+            const users = await User.all()
+            // Jika User selain role admin yang minta data user yang keluar cuma nama 
+            if(user?.role !== 'admin') {
+                return response.json({ users: users.map(user => ({ name: user.name })) })
+            }    
+            return response.status(200).json(users)
+        } catch (error) {
+            return response.status(500).json({ message: 'Internal server error' })
+        }
     }
 
     // Get User By Id
-    public async show({ params, response}: HttpContext) {
-        // const user = auth.user
-        // if(user?.role !== 'admin' && user?.id !== Number(params.id)) {
-        //     return response.status(403).json({ message: 'You are not allowed to access this resource' })
-        // }
-        const userData = await User.find(params.id)
-        if(!userData) {
-            return response.status(404).json({ message: 'User not found' })
+    public async show({ params, response, auth}: HttpContext) {
+        try {
+            const user = auth.user
+            const userData = await User.find(params.id)
+            if(!userData) {
+                return response.status(404).json({ message: 'User not found' })
+            }
+    
+            // Jika User selain role admin yang minta data user yang keluar cuma nama 
+            if(user?.role !== 'admin') {
+                return response.json({userData:{
+                    name: userData.name,
+                }})
+            }
+            return response.json(userData)
+        } catch (error) {
+            return response.status(500).json({ message: 'Internal server error' })
         }
-        return response.json(userData)
     }
 
     // Create User
@@ -36,7 +49,7 @@ export default class UsersController {
 
     // Login User
     public async login({ auth, request, response }: HttpContext) {
-        const { email, password } = request.body()
+        const { email, password } = request.only(['email', 'password'])
 
         const user = await User.findBy('email', email)
         if(!user) {
@@ -58,26 +71,47 @@ export default class UsersController {
         }
 
     // Update User
-    public async update({ params, request, response }: HttpContext) {
-        const user = await User.find(params.id)
-        if(!user) {
-            return response.status(404).json({ message: 'User not found' })
-        }
-        const data = request.only(['name', 'email', 'password'])
-        user.merge(data)
-        await user.save()
-
-        return response.json(user)
+    public async update({ params, request, response, auth}: HttpContext) {
+        try {
+            const user = auth.user
+            const userData = await User.find(params.id)
+            if(!userData) {
+                return response.status(404).json({ message: 'User not found' })
+            }
+            if(user?.id !== userData.id) {
+                return response.status(401).json({ message: 'Unauthorized' })
+            }
+            const data = request.only(['name', 'email', 'password'])
+            userData.merge(data)
+            await userData.save()
+    
+            return response.json({message: "user updated succesfully", user: {id: userData.id, name: userData.name, email: userData.email}})    
+        } catch (error) {
+            
+        } 
     }
 
     // Delete User
-    public async destroy({ params, response }: HttpContext) {
-        const user = await User.find(params.id)
-        if(!user) {
-            return response.status(404).json({ message: 'User not found' })
+    public async destroy({ params, response , auth}: HttpContext) {
+        try {
+            const user = await User.find(auth.user?.id)
+            const userToDelete = await User.find(params.id)
+            if(!userToDelete) {
+                return response.status(404).json({ message: 'User not found' })
+            }
+            // User sendiri yang mau ba hapus
+            if(user?.id === userToDelete.id) {
+                await userToDelete.delete()
+                return response.status(202).json({message: 'User self deleted successfully'})
+            }
+            // User lain yang mau hapus
+            if(user?.role !== 'admin') {
+                return response.status(401).json({ message: 'Unauthorized' })
+            }
+            await userToDelete.delete()
+            return response.status(202).json({message: 'User deleted successfully'})
+        } catch (error) {
+            return response.status(500).json({ message: 'Internal server error' })
         }
-        await user.delete()
-
-        return response.status(204).json({'message': 'User deleted successfully'})
     }
 }
