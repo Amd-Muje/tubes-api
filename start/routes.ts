a71e0e738aabe9ee8b56fec88ce131bb964aac67
ahmadmujahid <ahmadmujahid343@gmail.com>
2025-03-12 23:55:04 +0800

    migrasi ke react

--- start/routes.ts
+++ start/routes.ts
@@ -7,19 +7,22 @@ import TransactionsController from '#controllers/transactions_controller'
 
 // router.on('/').render('pages/home')
 
-router.group(() => {
+router
+  .group(() => {
     router.get('/campaigns', [CampaignsController, 'index']).middleware(middleware.auth())
     router.get('/campaigns/:id', [CampaignsController, 'show']).middleware(middleware.auth())
-    router.post('/campaigns', [CampaignsController,'store']).middleware(middleware.auth())
-    router.put('/campaigns/:id', [CampaignsController, 'updateStatus']).middleware(middleware.auth())
-    router.delete('/campaigns/:id', [CampaignsController,'destroy']).middleware(middleware.auth())
+    router.post('/campaigns', [CampaignsController, 'store']).middleware(middleware.auth())
+    router
+      .put('/campaigns/:id', [CampaignsController, 'updateStatus'])
+      .middleware(middleware.auth())
+    router.delete('/campaigns/:id', [CampaignsController, 'destroy']).middleware(middleware.auth())
 
     router.get('/users', [UsersController, 'index']).middleware(middleware.auth())
     router.get('/users/:id', [UsersController, 'show']).middleware(middleware.auth())
-    router.post('/users', [UsersController ,'store'])
-    router.post('/users/login', [UsersController ,'login'])
+    router.post('/users', [UsersController, 'store'])
+    router.post('/users/login', [UsersController, 'login'])
     router.put('/users/:id', [UsersController, 'update']).middleware(middleware.auth())
-    router.delete('/users/:id', [UsersController ,'destroy']).middleware(middleware.auth())
+    router.delete('/users/:id', [UsersController, 'destroy']).middleware(middleware.auth())
 
     router.get('/donations', [DonationsController, 'index']).middleware(middleware.auth())
     router.get('/donations/:id', [DonationsController, 'show']).middleware(middleware.auth())
@@ -30,6 +33,12 @@ router.group(() => {
     router.get('/transactions', [TransactionsController, 'index']).middleware(middleware.auth())
     router.get('/transactions/:id', [TransactionsController, 'show']).middleware(middleware.auth())
     router.post('/transactions', [TransactionsController, 'store']).middleware(middleware.auth())
-    router.put('/transactions/:id', [TransactionsController, 'update']).middleware(middleware.auth())
-    router.delete('/transactions/:id', [TransactionsController, 'destroy']).middleware(middleware.auth())
-}).prefix('/api/')
+    router
+      .put('/transactions/:id', [TransactionsController, 'update'])
+      .middleware(middleware.auth())
+    router
+      .delete('/transactions/:id', [TransactionsController, 'destroy'])
+      .middleware(middleware.auth())
+  })
+  .prefix('/api/')
+router.on('/').renderInertia('home')
